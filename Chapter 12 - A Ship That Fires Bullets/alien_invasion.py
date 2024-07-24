5466d0155fe3bfd3095b573d048a0ea86357e476
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2024-07-24 17:09:05 +0200

    changed something in AlienInvasion.py

--- Chapter 12 - A Ship That Fires Bullets/alien_invasion.py
+++ Chapter 12 - A Ship That Fires Bullets/alien_invasion.py
@@ -11,10 +11,16 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        # per far andare il gioco ugualmente veloce in tutti i sistemi e computers
+        self.clock = pygame.time.Clock()  
 
 
-        self.screen = pygame.dislpay.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+
+        # Set the background color.
+        self.bg_color = (230,230,230)
 
 
     def run_game(self):
@@ -24,10 +30,15 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                
 
+                
+                # Redraw the screen during each pass through the loop.
+                self.screen.fill(self.bg_color)
+                
+                
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
+                self.clock.tick(60)
 
 
 if __name__=='__main__':
