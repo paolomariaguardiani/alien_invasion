e4a5575fd98aab4391c4de1055ffb4fef7fffc6b
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2024-08-04 15:22:21 +0200

    added some features

--- Chapter 12 - A Ship That Fires Bullets/ship.py
+++ Chapter 12 - A Ship That Fires Bullets/ship.py
@@ -8,6 +8,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -19,6 +20,10 @@ class Ship:
         self.rect.midbottom = self.screen_rect.midbottom
 
 
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
+
         # Movement flag; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
@@ -26,11 +31,17 @@ class Ship:
     
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            # print(self.rect.right, self.screen_rect.right)
+
+            self.x += self.settings.ship_speed
         """If we use elif instead of if the right arrow key would always have priority."""
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
 
     def blitme(self):
