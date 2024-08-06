56bc268fb0adf4c9286d608cd3f2873046e7559b
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2024-08-06 17:16:47 +0200

    added bullets

--- Chapter 12 - A Ship That Fires Bullets/alien_invasion.py
+++ Chapter 12 - A Ship That Fires Bullets/alien_invasion.py
@@ -6,7 +6,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game, assets and behavior."""
@@ -34,6 +34,7 @@ class AlienInvasion:
 
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
         # Set the background color.
@@ -45,6 +46,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
             
@@ -69,6 +71,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
 
     def _check_keyup_events(self, event):
@@ -79,9 +83,17 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
 
